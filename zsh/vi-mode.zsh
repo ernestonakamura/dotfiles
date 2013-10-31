66a46b1bda3e3a2e5919e87a8eec85621b12ff1a
Ernesto Nakamura <ernesto@ernesto-macbook.local>
2013-10-31 17:15:11 -0200

    Customize zsh vi-mode

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -1,3 +1,12 @@
 set -o vi
 export EDITOR=vim
 export VISUAL=vim
+
+#http://ivyl.0xcafe.eu/2013/02/03/refining-zsh/
+export KEYTIMEOUT=1
+# ctrl-p ctrl-n history navigation
+bindkey '^P' up-history
+bindkey '^N' down-history
+# backspace and ^h working even after returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
