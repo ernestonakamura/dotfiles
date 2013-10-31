eb9935dd59031e475a652a64a2ed4c61b6f56e15
Ernesto Nakamura <ernesto@naka.eti.br>
2013-10-31 19:05:46 -0200

    Removing --color from ls alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,7 +21,7 @@ alias cdb='cd -'
 # Show human friendly numbers and colors
 alias df='df -h'
 alias ll='ls -alGh'
-alias ls='ls -Gh --color'
+alias ls='ls -Gh'
 alias du='du -h -d 2'
 
 # show me files matching "ls grep"
